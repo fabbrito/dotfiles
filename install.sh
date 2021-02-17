b64e05401740a2674d84cfea55fecb5882422e34
Fabrício <fab.brito94@gmail.com>
2021-02-17 19:14:44 -0300

    First commit of dotfiles

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+cd "$(dirname "${BASH_SOURCE[0]}")" || exit
+
+# Get the latest version of stowsh
+[ ! -f ./bin/stowsh ] && curl -o ./bin/stowsh https://raw.githubusercontent.com/mikepqr/stowsh/master/stowsh
+chmod +x ./bin/stowsh
+
+# Packages are non-hidden directories
+pkgs="$(find . -maxdepth 1 ! -name '.*' -type d | sed "s|./||")"
+
+for pkg in $pkgs
+do
+    ./bin/stowsh -v -s "$pkg" -t "$HOME"
+done
