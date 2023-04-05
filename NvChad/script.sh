d166f30aa38a734122b98185c5d9d09348dd654f
= <=>
2023-04-05 00:10:38 -0300

    fix: check if exists a nvim config folder

--- NvChad/script.sh
+++ NvChad/script.sh
@@ -1,20 +1,26 @@
 #!/usr/bin/env bash
 
+NVIMPATH="$HOME/.config/nvim"
+
+if [[ ! -d ${NVIMPATH} ]]; then
+  echo "~/.config/nvim/ not found"
+  exit 0
+fi
+
 SCRIPTPATH="$(realpath "${BASH_SOURCE[0]}")"
 DIRPATH="$(dirname ${SCRIPTPATH})"
 
 case "$1" in
 "update" | "u")
-  echo "from: .dotfiles -> to: .config"
-	cp -r -u ${DIRPATH}/custom ~/.config/nvim/lua/custom
+	echo "from: .dotfiles -> to: .config"
+	cp -r -u ${DIRPATH}/custom ${NVIMPATH}/lua/custom
 	;;
 "retrieve" | "r")
 	echo "from: .config   -> to: .dotfiles"
-	cp -r -u ~/.config/nvim/lua/custom ${DIRPATH}/custom
+	cp -r -u ${NVIMPATH}/lua/custom ${DIRPATH}/custom
 	;;
 *)
-  echo "script.sh [update|u]   - copy and update files from: .dotfiles -> to: .config"
-  echo "script.sh [retrieve|r] - copy and update files from: .config   -> to: .dotfiles"
+	echo "script.sh [update|u]   - copy and update files from: .dotfiles -> to: .config"
+	echo "script.sh [retrieve|r] - copy and update files from: .config   -> to: .dotfiles"
 	;;
 esac
-
