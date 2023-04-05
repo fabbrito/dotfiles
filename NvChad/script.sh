7c436b67586d5b3a94fee316d9d4190a89f00a15
= <=>
2023-04-05 01:03:12 -0300

    fix: change from using cp to rsync

--- NvChad/script.sh
+++ NvChad/script.sh
@@ -3,8 +3,8 @@
 NVIMPATH="$HOME/.config/nvim"
 
 if [[ ! -d ${NVIMPATH} ]]; then
-  echo "~/.config/nvim/ not found"
-  exit 0
+	echo "~/.config/nvim/ not found"
+	exit 0
 fi
 
 SCRIPTPATH="$(realpath "${BASH_SOURCE[0]}")"
@@ -12,12 +12,12 @@ DIRPATH="$(dirname ${SCRIPTPATH})"
 
 case "$1" in
 "update" | "u")
-	echo "from: .dotfiles -> to: .config"
-	cp -r -u ${DIRPATH}/custom ${NVIMPATH}/lua/custom
+	read -p 'from: .dotfiles -> to: .config'
+	rsync -aq ${DIRPATH}/custom/ ${NVIMPATH}/lua/custom
 	;;
 "retrieve" | "r")
-	echo "from: .config   -> to: .dotfiles"
-	cp -r -u ${NVIMPATH}/lua/custom ${DIRPATH}/custom
+	read -p 'from: .config   -> to: .dotfiles'
+	rsync -aq ${NVIMPATH}/lua/custom/ ${DIRPATH}/custom
 	;;
 *)
 	echo "script.sh [update|u]   - copy and update files from: .dotfiles -> to: .config"
