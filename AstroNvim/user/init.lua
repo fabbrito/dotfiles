1032e8be29733e59b05bf636eeb7dad0d3b384ff
Fabrício Brito <fab.brito94@gmail.com>
2023-04-06 18:37:32 -0300

    fix: smartcolumn plugin

--- AstroNvim/user/init.lua
+++ AstroNvim/user/init.lua
@@ -1,21 +1,22 @@
 return {
-  plugins = {
-    "AstroNvim/astrocommunity",
-    { import = "astrocommunity.pack.lua" },
-    { import = "astrocommunity.pack.python" },
-    { import = "astrocommunity.pack.rust" },
-    { import = "astrocommunity.pack.typescript" },
-    { import = "astrocommunity.pack.bash" },
-    { import = "astrocommunity.pack.markdown" },
-    { import = "astrocommunity.bars-and-lines.heirline-mode-text-statusline" },
-    { import = "astrocommunity.diagnostics.lsp_lines-nvim" },
-    { import = "astrocommunity.diagnostics.trouble-nvim" },
-    {
-      "m4xshen/smartcolumn.nvim",
-      ots = {
-        colorcolumn = 120,
-        disabled_filetypes = { "help" },
-      },
-    },
-  },
+	plugins = {
+		"AstroNvim/astrocommunity",
+		{ import = "astrocommunity.pack.lua" },
+		{ import = "astrocommunity.pack.python" },
+		{ import = "astrocommunity.pack.rust" },
+		{ import = "astrocommunity.pack.typescript" },
+		{ import = "astrocommunity.pack.bash" },
+		{ import = "astrocommunity.pack.markdown" },
+		{ import = "astrocommunity.bars-and-lines.heirline-mode-text-statusline" },
+		{ import = "astrocommunity.diagnostics.lsp_lines-nvim" },
+		{ import = "astrocommunity.diagnostics.trouble-nvim" },
+		{ import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
+		-- {
+		-- 	"m4xshen/smartcolumn.nvim",
+		-- 	opts = {
+		-- 		colorcolumn = "80",
+		-- 		disabled_filetypes = { "neo-tree", "lazy", "mason", "help" },
+		-- 	},
+		-- },
+	},
 }
