e670840c5347d9fb5c9d12544ea371d98179fe3e
Fabrício Brito <fab.brito94@gmail.com>
2023-04-11 11:06:22 -0300

    change: Statusline to NvChad style

--- AstroNvim/user/init.lua
+++ AstroNvim/user/init.lua
@@ -1,3 +1,79 @@
 return {
+  -- colorscheme = "nightfox",
   colorscheme = "catppuccin",
+  lsp = {
+    -- customize lsp formatting options
+    formatting = {
+      timeout_ms = 2000, -- default format timeout
+    },
+  },
+  icons = {
+    VimIcon = "",
+    ScrollText = "",
+    MacroRecording = "",
+    ActiveLSP = "",
+    ActiveTS = "綠",
+    BufferClose = "",
+    NeovimClose = "",
+    DefaultFile = "",
+    Diagnostic = "裂",
+    DiagnosticError = "",
+    DiagnosticHint = "",
+    DiagnosticInfo = "",
+    DiagnosticWarn = "",
+    Ellipsis = "…",
+    FileModified = "",
+    FileReadOnly = "",
+    FolderClosed = "",
+    FolderEmpty = "",
+    FolderOpen = "",
+    Git = "",
+    GitAdd = "",
+    GitBranch = "",
+    GitChange = "",
+    GitConflict = "",
+    GitDelete = "",
+    GitIgnored = "◌",
+    GitRenamed = "➜",
+    GitStaged = "✓",
+    GitUnstaged = "✗",
+    GitUntracked = "★",
+    LSPLoaded = "",
+    LSPLoading1 = "",
+    LSPLoading2 = "",
+    LSPLoading3 = "",
+  },
+  -- modify variables used by heirline but not defined in the setup call directly
+  heirline = {
+    -- define the separators between each section
+    separators = {
+      left = { "", " " }, -- separator for the left side of the statusline
+      right = { " ", "" }, -- separator for the right side of the statusline
+      -- tab = { "", "" },
+    },
+    -- add new colors that can be used by heirline
+    colors = function(hl)
+      local get_hlgroup = require("astronvim.utils").get_hlgroup
+      -- use helper function to get highlight group properties
+      local comment_fg = get_hlgroup("Comment").fg
+      hl.git_branch_fg = comment_fg
+      hl.git_added = comment_fg
+      hl.git_changed = comment_fg
+      hl.git_removed = comment_fg
+      hl.blank_bg = get_hlgroup("Folded").fg
+      hl.file_info_bg = get_hlgroup("Visual").bg
+      hl.nav_icon_bg = get_hlgroup("String").fg
+      hl.nav_fg = hl.nav_icon_bg
+      hl.folder_icon_bg = get_hlgroup("Error").fg
+      return hl
+    end,
+    attributes = {
+      mode = { bold = true },
+    },
+    icon_highlights = {
+      file_icon = {
+        statusline = false,
+      },
+    },
+  },
 }
